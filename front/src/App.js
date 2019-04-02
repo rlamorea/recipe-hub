d4e4bd26ffb3cc9612e70583561a7b37773f5063
Rick Lamoreaux <rlamorea@gmail.com>
2019-04-01 23:58:29 -0700

    fixes #1 - react and node and feathers set up

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        recipes: []
+    };
+
+    componentDidMount() {
+        this.fetchRecipes();
+    }
+
+    fetchRecipes = () => {
+        axios.get('http://localhost:3030/recipes')
+            .then( resp => this.setState({ recipes: resp.data || [] }));
+    };
+
+    render() {
+        const { recipes } = this.state;
+        console.log(recipes);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>
+                        Recipes:
+                    </h2>
+                    <ul>
+                        { recipes.length <= 0
+                            ? <li>No Recipes Found</li>
+                            : recipes.map(recipe => (
+                                <li>{recipe.title}</li>
+                            ))
+                        }
+                    </ul>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
